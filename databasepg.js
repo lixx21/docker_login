6466699b03249ddf753e1396b1a7774bf65149a6
Felix Pratamasan <91602612+lixx21@users.noreply.github.com>
2023-01-16 10:05:21 -0800

    update databasepg.js host

--- databasepg.js
+++ databasepg.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: 'localhost',
+    host: 'docker_login_db_1',
     user: 'postgres',
     port: 5432,
     password: '123456',
