f7dccca7732c7186f3d3db21b513f9d253a6551e
Felix <felixpratama242@gmail.com>
2023-01-22 23:37:17 +0800

    update databasepg.js

--- databasepg.js
+++ databasepg.js
@@ -1,12 +1,23 @@
-const {Client} = require('pg')
+// const {Client} = require('pg')
+
+// const client = new Client({
+//     // host: 'docker_login_db_1', // for docker
+//     // host: 'localhost',
+//     // user: 'postgres',
+//     // port: 5432,
+//     // password: '123456',
+//     // database: 'postgres'
+
+//     // use postgres production in railway
+// });
+
+const { Client } = require("pg");
+
+const connectionString =
+  "postgresql://postgres:JAnsITlNkokTasO8fzkQ@containers-us-west-155.railway.app:7049/railway";
 
 const client = new Client({
-    // host: 'docker_login_db_1', // for docker
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: '123456',
-    database: 'postgres'
-})
+  connectionString,
+});
 
 module.exports = client
